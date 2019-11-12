e64ae6de47637682a678b5b7ce3a90b2d34486ae
John Lindquist <johnlindquist@gmail.com>
2019-11-12 12:41:16 -0700

    Tue Nov 12 12:41:16 MST 2019

--- src/routes/images.json.js
+++ src/routes/images.json.js
@@ -1,9 +1,9 @@
-const fs = require("fs").promises
-const path = require("path")
+const fs = require("fs").promises;
+const path = require("path");
 
 export async function get(req, res, next) {
   const images = await fs.readdir(
     path.join(__dirname, `../../../static/images`)
-  )
-  res.end(JSON.stringify(images.map(image => `images/${image}`)))
+  );
+  res.end(JSON.stringify(images.map(image => `images/${image}`).reverse()));
 }
